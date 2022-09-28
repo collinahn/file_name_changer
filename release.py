8d2a9b3ed91e6f33b1fafdfaea9e42bfccbe7d06
Collin Ahn <collinahn@hufs.ac.kr>
2022-09-28 23:02:13 +0900

    update enable release automation without pw

--- release.py
+++ release.py
@@ -2,11 +2,12 @@
 # 2. 버전 이름과 같은 exe파일이 ~/change-file-name-api-server/release/ 폴더 내에 위치해야 함.
 import paramiko
 from scp import SCPClient, SCPException
-import getpass 
+import getpass
 import os
 
 from qWordBook import VERSION_INFO
 from lib.__PRIVATE import IP, PORT_SSH
+
 
 class SSHManager:
     """ 
@@ -18,43 +19,44 @@ class SSHManager:
         >>> ssh_manager.send_file("/path/to/local_path", "/path/to/remote_path") 
         >>> ssh_manager.get_file("/path/to/remote_path", "/path/to/local_path") ... 
         >>> ssh_manager.close_ssh_client() 
-    """ 
-    
-    def __init__(self): 
-        self.ssh_client = None 
-        
-    def create_ssh_client(self, hostname, portname, username, password): 
-        """Create SSH client session to remote server""" 
-        if self.ssh_client is None: 
-            self.ssh_client = paramiko.SSHClient() 
-            self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
-            self.ssh_client.connect(hostname, portname, username, password) 
-        else: 
-            print("SSH client session exist.") 
+    """
 
-    def close_ssh_client(self): 
-        """Close SSH client session""" 
-        self.ssh_client.close() 
+    def __init__(self):
+        self.ssh_client = None
 
-    def send_file(self, local_path, remote_path): 
-        """Send a single file to remote path""" 
-        try: 
+    def create_ssh_client(self, hostname, portname, username, password=None):
+        """Create SSH client session to remote server"""
+        if self.ssh_client is None:
+            self.ssh_client = paramiko.SSHClient()
+            self.ssh_client.set_missing_host_key_policy(
+                paramiko.AutoAddPolicy())
+            self.ssh_client.connect(hostname, portname, username, password)
+        else:
+            print("SSH client session exist.")
+
+    def close_ssh_client(self):
+        """Close SSH client session"""
+        self.ssh_client.close()
+
+    def send_file(self, local_path, remote_path):
+        """Send a single file to remote path"""
+        try:
             with SCPClient(self.ssh_client.get_transport()) as scp:
-                scp.put(local_path, remote_path, preserve_times=True) 
-        except SCPException: 
-            raise SCPException.message 
-    
-    def get_file(self, remote_path, local_path): 
-        """Get a single file from remote path""" 
-        try: 
-            with SCPClient(self.ssh_client.get_transport()) as scp: 
-                scp.get(remote_path, local_path) 
-        except SCPException: 
-            raise SCPException.message 
-        
-    def send_command(self, command): 
-        """Send a single command""" 
-        stdin, stdout, stderr = self.ssh_client.exec_command(command) 
+                scp.put(local_path, remote_path, preserve_times=True)
+        except SCPException:
+            raise SCPException.message
+
+    def get_file(self, remote_path, local_path):
+        """Get a single file from remote path"""
+        try:
+            with SCPClient(self.ssh_client.get_transport()) as scp:
+                scp.get(remote_path, local_path)
+        except SCPException:
+            raise SCPException.message
+
+    def send_command(self, command):
+        """Send a single command"""
+        stdin, stdout, stderr = self.ssh_client.exec_command(command)
         return stdout.readlines()
 
 
@@ -70,15 +72,13 @@ if __name__ == '__main__':
     print('file updated with', VERSION_INFO)
     print('getting ready to a scp session')
 
-    pw = getpass.getpass('enter passwd for ssh: ')
-
     buildRes = os.system('pyinstaller -F --clean qMain.spec')
     if buildRes != 0:
         print('build failed')
         raise(RuntimeError)
 
     ssh_manager = SSHManager()
-    ssh_manager.create_ssh_client(IP, PORT_SSH, 'pi', pw)
+    ssh_manager.create_ssh_client(IP, PORT_SSH, 'pi')
 
     targetFile = './dist/qMain.exe'
     dstFileName = f'./dist/{VERSION_INFO}.exe'
@@ -91,8 +91,18 @@ if __name__ == '__main__':
     os.rename(targetFile, dstFileName)
     print(f'file renamed to ./dist/{VERSION_INFO}.exe, preparing to send')
 
-    ssh_manager.send_file('./version.txt', '/home/pi/change-file-name-api-server/')
-    ssh_manager.send_file(f'./dist/{VERSION_INFO}.exe', '/home/pi/change-file-name-api-server/release/')
+    usr_input = input('do you proceed? ')
+    if usr_input != 'y':
+        print('=============================')
+        print(VERSION_INFO, 'did not released')
+        print('=============================')
+        import sys
+        sys.exit()
+
+    ssh_manager.send_file(
+        './version.txt', '/home/pi/change-file-name-api-server/')
+    ssh_manager.send_file(
+        f'./dist/{VERSION_INFO}.exe', '/home/pi/change-file-name-api-server/release/')
     print('transfer complete')
 
     ssh_manager.close_ssh_client()
